425970c6e6c5262e3d3dedd41a748263ba3944e4
Eduard Podaru <eduard.podaru@polymtlus.ca>
2025-09-04 15:43:23 -0400

    feat(admin): creation de la liste de jeu ainsi que de l'objet jeu

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,7 +5,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
-import { CreatePageComponent } from ''; //todo
+//import { CreatePageComponent } from ''; todo
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -19,7 +19,7 @@ if (environment.production) {
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'admin', component: AdminPageComponent },
-    { path: 'create', component: CreatePageComponent },
+    //{ path: 'create', component: CreatePageComponent },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
