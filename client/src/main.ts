480c381ea9b17d3cb499ce2651c7c288312de375
Eduard Podaru <eduard.podaru@polymtlus.ca>
2025-09-04 14:14:26 -0400

    feat(admin): creation de la page pour l'administration

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -16,6 +17,7 @@ if (environment.production) {
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: 'admin', component: AdminPageComponent},
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
