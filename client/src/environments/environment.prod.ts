805eda72e32302dc24c53b7a7aa759681763b6bc
Adam Raf <you@example.com>
2025-09-19 23:34:03 -0400

    changement variables environnements

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-votre-ip-ici.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
-    socketUrl: 'http://ec2-votre-ip-ici.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote socket URL
+    serverUrl: 'http://ec2-35.183.68.135.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
+    socketUrl: 'http://ec2-35.183.68.135.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote socket URL
 };
