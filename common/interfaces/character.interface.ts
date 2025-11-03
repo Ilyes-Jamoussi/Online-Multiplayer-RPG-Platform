9e35066a553a6de06c7df70807696e07cf38ccba
Ed_pod <eduard.podaru@etud.polymtl.ca>
2025-11-03 17:31:31 -0500

    fix(character interface): correction des methode qui utilisait des objet anonymes pour des interfaces

--- common/interfaces/character.interface.ts
+++ common/interfaces/character.interface.ts
@@ -1,11 +1,12 @@
 import { Avatar } from '@common/enums/avatar.enum';
 import { BonusType } from '@common/enums/character-creation.enum';
-import { Dice } from '@common/enums/dice.enum';
+import { DiceAssignment } from './dice-assignement.interface';
+import { Attributes } from './attributes.interface';
 
 export interface Character {
     name: string;
     avatar: Avatar | null;
     bonus: BonusType | null;
-    diceAssignment: { attack: Dice; defense: Dice };
-    attributes: { life: number; speed: number };
+    diceAssignment: DiceAssignment;
+    attributes: Attributes;
 }
