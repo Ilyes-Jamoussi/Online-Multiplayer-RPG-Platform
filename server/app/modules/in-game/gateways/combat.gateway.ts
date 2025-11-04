3090cd7a78f693bcabead5749aea1a5ed16fdfb1
IlyesJamoussi <jamoussi.mail@gmail.com>
2025-11-04 14:21:07 -0500

    Suppression des logs du serveur

--- server/app/modules/in-game/gateways/combat.gateway.ts
+++ server/app/modules/in-game/gateways/combat.gateway.ts
@@ -3,7 +3,7 @@ import { errorResponse, successResponse } from '@app/utils/socket-response/socke
 import { CombatPosture } from '@common/enums/combat-posture.enum';
 import { InGameEvents } from '@common/enums/in-game-events.enum';
 import { CombatResult } from '@common/interfaces/combat.interface';
-import { Injectable, Logger, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Injectable, UsePipes, ValidationPipe } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
@@ -12,7 +12,6 @@ import { Server, Socket } from 'socket.io';
     new ValidationPipe({
         transform: true,
         exceptionFactory: (errors): Error => {
-            new Logger('ValidationPipe').error('Validation failed:', errors);
             throw new Error('Validation failed');
         },
     }),
@@ -21,7 +20,6 @@ import { Server, Socket } from 'socket.io';
 @Injectable()
 export class CombatGateway {
     @WebSocketServer() private readonly server: Server;
-    private readonly logger = new Logger(CombatGateway.name);
 
     constructor(private readonly combatService: CombatService) {}
 
@@ -78,7 +76,6 @@ export class CombatGateway {
     handleCombatTimerRestart(payload: { sessionId: string }): void {
         const session = this.combatService.getSession(payload.sessionId);
         if (session) {
-            this.logger.log(`Combat timer restarted for session ${session.id}`);
             this.server.to(session.inGameId).emit(InGameEvents.CombatTimerRestart, successResponse({}));
         }
     }
