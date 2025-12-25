b52b97aa5644cfbc135daca7bb0043bc07390c89
IlyesJamoussi <jamoussi.mail@gmail.com>
2025-12-25 03:29:50 +0100

    Translate map size labels to English

--- common/constants/game.constants.ts
+++ common/constants/game.constants.ts
@@ -1,11 +1,11 @@
 import { MapSize } from '@common/enums/map-size.enum';
 
-export const DEFAULT_DRAFT_GAME_NAME = 'Nom...';
-export const DEFAULT_DRAFT_GAME_DESCRIPTION = 'Description du jeu...';
+export const DEFAULT_DRAFT_GAME_NAME = 'Name...';
+export const DEFAULT_DRAFT_GAME_DESCRIPTION = 'Game description...';
 export const BOAT_SPEED_BONUS = 4;
 
 export const MAP_SIZE_LABELS: Record<MapSize, string> = {
-    [MapSize.SMALL]: 'Petit : 10x10',
-    [MapSize.MEDIUM]: 'Moyen : 15x15',
-    [MapSize.LARGE]: 'Grand : 20x20',
+    [MapSize.SMALL]: 'Small: 10x10',
+    [MapSize.MEDIUM]: 'Medium: 15x15',
+    [MapSize.LARGE]: 'Large: 20x20',
 };
