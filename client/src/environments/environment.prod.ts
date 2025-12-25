19805692aa6bea1d4b28703159f6b03b5e32f26b
IlyesJamoussi <jamoussi.mail@gmail.com>
2025-12-25 18:54:15 +0100

    Update backend URL to use EC2 instance and secure SSH key

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = {
     production: true,
-    serverUrl: 'https://rpg-platform-backend.onrender.com/api',
-    socketUrl: 'https://rpg-platform-backend.onrender.com',
+    serverUrl: 'http://13.60.84.0:3000/api',
+    socketUrl: 'http://13.60.84.0:3000',
 };
